73c67d7316602dbe26cc46a42ac74a8aecdd79cd
ereminiu <ereminiu26@yandex.ru>
2023-07-26 13:53:20 +0300

    Add custom code table in migration schema

--- schema.sql
+++ schema.sql
@@ -2,6 +2,9 @@ drop table if exists link_to_code;
 drop table if exists links;
 drop table if exists codes;
 
+drop table if exists link_to_custom_code;
+drop table if exists custom_codes;
+
 create table links (
     id serial primary key,
     link varchar(255)
@@ -18,3 +21,15 @@ create table link_to_code (
     code_id int not null references codes,
     unique (link_id, code_id)
 );
+
+create table custom_codes (
+    id serial primary key,
+    code varchar(10)
+);
+
+create table link_to_custom_code (
+    id serial primary key,
+    link_id int not null references links,
+    code_id int not null references custom_codes,
+    unique (link_id, code_id)
+)
