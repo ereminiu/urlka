18b399d2c043054a92356d159dafa6550945cdca
ereminiu <ereminiu26@yandex.ru>
2023-07-27 12:16:53 +0300

    Add /addlink handler

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ from loguru import logger
 from typing import Tuple
 
 from flask import Flask
+from flask import request
+from models import models
 
 repos = Repository()
-Service = Service(repos)
+service = Service(repos)
 
 app = Flask(__name__)
 
@@ -16,4 +18,17 @@ def reinit() -> Tuple[str, int]:
     repos.reinit()
     return "migrations applied", 200
 
+@app.route("/addlink", methods=["POST"])
+def add_link() -> Tuple[str, int]:
+    inp = models.LinkRequest.model_validate_json(request.data)
+    
+    # TODO: check whether link is valid
+    # TODO: check whether custom_code isn't used before
+
+    code = service.add_link(inp.link)
+    resp = models.CodeResponse(code=code, \
+                               msg="Link has been added,").model_dump_json()
+    return resp, 200
+
+app.run(host="localhost", port=8000, debug=True)
 repos.close_db()
